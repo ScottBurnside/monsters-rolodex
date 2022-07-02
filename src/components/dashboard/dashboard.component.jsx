d046371042ef9732ad7693720bbc02fb1046ed91
andreyhapal <andreyrapterror@mail.ru>
2022-07-02 20:09:09 +0400

    added some graphs and buttons

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -10,11 +10,28 @@ export const Dashboard = (props) => (
         <h1>Dashboard</h1>
         <div class="dashboard-card">
             <div class="dashboard-card-header">
-                <span><b>Jira</b></span>
+                <div class="header-text">
+                    <span>Jira</span>
+                </div>
+                <div class="team-button">
+                    <span>ELK</span>
+                </div>
+                <div class="team-button">
+                    <span>Gizmo</span>
+                </div>
+                <div class="team-button">
+                    <span>Platform</span>
+                </div>
+                <div class="team-button">
+                    <span>ELK</span>
+                </div>
+                <div class="team-button">
+                    <span>Gizmo</span>
+                </div>
             </div>
             <div class="dashboard-card-content">
                 <div class="info-content content-child">
-                    <span>Sprint Date</span><br/>
+                    <span>Sprint Date</span><br />
                     <span>Sprint Goals</span>
                     <ul>
                         <li>goal1</li>
@@ -47,7 +64,7 @@ export const Dashboard = (props) => (
                             label: 'My First Dataset',
                             data: [65, 59, 80, 81, 56, 55, 40],
                             fill: false,
-                            borderColor: 'rgb(75, 192, 192)',
+                            borderColor: '#ff7f00',
                             tension: 0.1
                         }]
                     }}
@@ -58,7 +75,7 @@ export const Dashboard = (props) => (
                         }} />
                 </div>
                 <div class="content-child">
-                <Pie data={{
+                    <Pie data={{
                         labels: ['Bugs', 'Features', 'Other'],
                         datasets: [{
                             label: 'My First Dataset',
@@ -80,15 +97,125 @@ export const Dashboard = (props) => (
         </div>
         <div class="dashboard-card">
             <div class="dashboard-card-header">
-                <span><b>Sonar</b></span>
+                <div class="header-text">
+                    <span>Reg</span>
+                </div>
+                <div class="team-button">
+                    <span>SSO</span>
+                </div>
+                <div class="team-button">
+                    <span>Activity</span>
+                </div>
+                <div class="team-button">
+                    <span>F&D</span>
+                </div>
+                <div class="team-button">
+                    <span>Payment</span>
+                </div>
+                <div class="team-button">
+                    <span>Reg</span>
+                </div>
             </div>
-            <div class="dashboard-card-content"></div>
+            <div class="dashboard-card-content">
+                <div class="content-child">
+                    <Line data={{
+                        labels: [1, 2, 3, 4, 5],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            fill: false,
+                            borderColor: '#ff7f00',
+                            tension: 0.1
+                        }]
+                    }}
+                        height={200}
+                        width={300}
+                        options={{
+                            maintainAspectRatio: false,
+                        }} />
+                </div>
+                <div class="content-child">
+                    <Line data={{
+                        labels: [1, 2, 3, 4, 5],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            fill: false,
+                            borderColor: '#ff7f00',
+                            tension: 0.1
+                        }]
+                    }}
+                        height={200}
+                        width={300}
+                        options={{
+                            maintainAspectRatio: false,
+                        }} />
+                </div>
+                <div class="content-child">
+                    <Line data={{
+                        labels: [1, 2, 3, 4, 5],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            fill: false,
+                            borderColor: '#ff7f00',
+                            tension: 0.1
+                        }]
+                    }}
+                        height={200}
+                        width={300}
+                        options={{
+                            maintainAspectRatio: false,
+                        }} />
+                </div>
+                <div class="content-child">
+                    stats
+                </div>
+            </div>
         </div>
         <div class="dashboard-card">
             <div class="dashboard-card-header">
-                <span><b>AWS</b></span>
+                <div class="header-text">
+                    <span>AWS</span>
+                </div>
+                <div class="team-button">
+                    <span>ELK</span>
+                </div>
+                <div class="team-button">
+                    <span>Gizmo</span>
+                </div>
+                <div class="team-button">
+                    <span>Platform</span>
+                </div>
+                <div class="team-button">
+                    <span>ELK</span>
+                </div>
+                <div class="team-button">
+                    <span>Gizmo</span>
+                </div>
             </div>
-            <div class="dashboard-card-content"></div>
+            <div class="dashboard-card-content">
+                <div class="content-child">
+                    <Line data={{
+                        labels: [1, 2, 3, 4, 5],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [65, 59, 80, 81, 56, 55, 40],
+                            fill: false,
+                            borderColor: '#ff7f00',
+                            tension: 0.1
+                        }]
+                    }}
+                        width={1000}
+                        height={200}
+                        options={{
+                            maintainAspectRatio: false,
+                        }} />
+                </div>
+                <div class="content-child">
+                    stats
+                </div>
+            </div>
         </div>
     </div>
 );
