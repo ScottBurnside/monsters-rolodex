023e002feada3d9897c12b52af870aa37064cb6d
andreyhapal <andreyrapterror@mail.ru>
2022-07-05 17:22:12 +0400

    tile link changed

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('https://e272-86-180-175-202.eu.ngrok.io/v1/costs')
-    //.then(response => response.json())
-    .then(data => this.setState({costsLabels: data.labels, costsValues: data.data}));
+    fetch('https://metrics.enthuse-test.com/v1/costs')
+    .then(response => response.json())
+    .then(data => this.setState({costsLabels: data[0].labels, costsValues: data[0].data}));
 
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://metrics.enthuse-test.com/v1/metrics')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = (props) => (
-    <div className= 'card-container' >
+    <div className= 'card-container' style={{backgroundColor: props.monster.rating}} >
         <img alt="monster" src={`https://res.cloudinary.com/snyk/image/upload/v1537345897/press-kit/brand/logo-vertical-black.png?set=set2&size=180x180`} />
-        <h2> {props.monster.username} </h2>
-        <p> {props.monster.email} </p> 
+        <h2> {props.monster.value} </h2>
+        <p> {props.monster.name} </p> 
     </div>
 );
