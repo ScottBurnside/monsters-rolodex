f97797ab12e65202c636c87e2acf2e7c9b8aa1b3
andreyhapal <andreyrapterror@mail.ru>
2022-07-05 18:19:59 +0400

    card changes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = (props) => (
-    <div className= 'card-container' style={{borderColor: props.monster?.rating}} >
+    <div className='card-container' style={{ borderColor: props.monster?.rating }} >
         <img alt="monster" src={`https://res.cloudinary.com/snyk/image/upload/v1537345897/press-kit/brand/logo-vertical-black.png?set=set2&size=180x180`} />
-        <h2> {props.monster?.value} </h2>
-        <p> {props.monster?.name} </p> 
+        <div>
+            <span className='card-value'> {props.monster?.value} </span><br/>
+            <span className='card-label'> {props.monster?.name} </span>
+        </div>
     </div>
 );
